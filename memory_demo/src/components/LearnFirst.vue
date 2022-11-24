6787845c9ae42f1fa13fdb42271a1e0d104727ea
entertain_c <2569942117@qq.com>
2022-11-24 22:40:29 +0800

    feat: 增加vue-JSX插件 & 配置

---
+++ memory_demo/src/components/LearnFirst.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+  import { ref } from 'vue';
+  const num = ref<number>(0);
+  const addNum = (): void => {
+    num.value ++;
+  }
+</script>
+
+<template>
+  <div>
+    点击次数: {{ num }}}
+  </div>
+  <button onclick="addNum">点击 + 1</button>
+</template>
+
+<style scoped>
+
+</style>
