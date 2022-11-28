1cecff5f77562b01cd3ae1d103d54f503c115a65
entertain_c <2569942117@qq.com>
2022-11-29 00:38:36 +0800

    feat: 增加备忘录表单组件

---
+++ memory_demo/src/components/DevComp/MemoForm/MemoForm.vue
@@ -0,0 +1,49 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+import { API } from '../../../constant/APIConstant';
+import { setData } from '../../../utils/Network';
+import MemoFormItem from './MemoFormItem/MemoFormItem.vue';
+import { v4 as uuidv4 } from 'uuid';
+
+const reqData = ref<any>({
+  text: '',
+  date: '',
+  time: '',
+});
+
+console.warn('value = ', reqData.value)
+const labelArr: string[] = ['内容', '日期', '时间'];
+const typeArr: string[] = ['text', 'date', 'time']
+const handleClick = (e: any, id: string): void => {
+  const value = {id: uuidv4(), ...reqData.value};
+  console.warn('提交value = ', value);
+  setData(API, value);
+}
+
+const handleItemValueChoose = (e: any, type: string): void => {
+  reqData.value[type] = e.target.value;
+  console.warn(reqData.value[type]);
+}
+
+</script>
+
+<template>
+  <div>
+    <div v-for='i in 3'>
+      <MemoFormItem :label='labelArr[i - 1]' :type='typeArr[i - 1]' :reqData='reqData' :handleItemValueChoose='e => handleItemValueChoose(e, typeArr[i - 1])'/>
+    </div>
+    <button class='btn' @click='handleClick'>
+      新增
+    </button>
+    <hr />
+  </div>
+</template>
+
+<style scoped>
+  .btn {
+    width: 100%;
+    height: 30px;
+    color: white;
+    background-color: black;
+  }
+</style>
---
+++ memory_demo/src/components/DevComp/MemoForm/MemoFormItem/MemoFormItem.vue
@@ -0,0 +1,35 @@
+<script setup lang='ts'>
+  import {defineProps} from 'vue';
+
+  interface MemoFormItemProps {
+    label: string;
+    type: string;
+    reqData: any;
+    handleItemValueChoose: (e: any) => void;
+  };
+
+  defineProps<MemoFormItemProps>();
+</script>
+
+<template>
+  <div class='container'>
+    <div class='label'>
+      {{ label }}
+    </div>
+    <input :type='type' @input='handleItemValueChoose' />
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 6px;
+  }
+
+  .label {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+</style>
