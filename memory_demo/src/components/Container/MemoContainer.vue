39b1fe0eefb909a03e84d9d7e62e993fbbe3715b
entertain_c <2569942117@qq.com>
2022-11-29 00:37:33 +0800

    feat: '增加备忘录的Container组件'

---
+++ memory_demo/src/components/Container/MemoContainer.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+import MemoDataList from '../DevComp/MemoDataList/MemoDataList.vue'
+import MemoForm from '../DevComp/MemoForm/MemoForm.vue'
+
+</script>
+
+<template>
+  <div class='container'>
+    <MemoForm />
+    <MemoDataList />
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    width: 400px;
+  }
+</style>
